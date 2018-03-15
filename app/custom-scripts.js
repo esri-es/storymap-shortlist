b5e4f1084663793e97a1fac8585205f6d5b5ea1f
RaulJimenezOrtega <hhkaos@gmail.com>
2018-03-15 19:50:55 +0100

    Deployable version

---
+++ app/custom-scripts.js
@@ -0,0 +1,31 @@
+define(["dojo/topic"], function(topic) {
+	/*
+	* Custom Javascript to be executed while the application is initializing goes here
+	*/
+
+	// The application is ready
+	topic.subscribe("tpl-ready", function(){
+		/*
+		* Custom Javascript to be executed when the application is ready goes here
+		*/
+	});
+
+    const strip = function (html)
+    {
+       var tmp = document.createElement("DIV");
+       tmp.innerHTML = html;
+       return tmp.textContent || tmp.innerText || "";
+    }
+
+    topic.subscribe("story-tab-changed", function(index){
+        console.log(`Index = ${index}`);
+        var data = {
+             hitType: 'event',
+             eventCategory: 'section',
+             eventAction: 'story-tab-navigation',
+             eventLabel: strip(app.data.getStory()[index].title).trim()
+        };
+        console.log('Sending data to GA: ', data);
+        ga('send', data);
+	});
+});
